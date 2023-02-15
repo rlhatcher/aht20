199f7f96b8656b7ce1b9763c08ca004482606481
Ronald Hatcher <ronaldhatcher@mac.com>
2023-02-15 23:32:01 +0000

    adding MQTT support

--- temp_hum.py
+++ temp_hum.py
@@ -1,28 +1,23 @@
 import AHT20
-import datetime
 import time
+import paho.mqtt.client as mqtt
+
+broker_address = "pifarmer.local"
+
+client = mqtt.Client("Farmer")
+client.connect(broker_address)
 
 # Initialize an AHT20
-aht20 = AHT20.AHT20()
 
-# A different bus can be specified with the BusNum variable
-# aht20 = AHT20.AHT20(BusNum = 1)
+aht20 = AHT20.AHT20()
 
 while 1:
 
     # Fill a string with date, humidity and temperature
-    data = str(datetime.datetime.now()) + ";" + "{:10.2f}".format(aht20.get_humidity()) + " %RH;" + "{:10.2f}".format(aht20.get_temperature()) + " °C"
-    # Data with crc8 check
-    data_crc8 = str(datetime.datetime.now()) + ";" + "{:10.2f}".format(aht20.get_humidity_crc8()) + " %RH;" + "{:10.2f}".format(aht20.get_temperature_crc8()) + " °C"
-
-    # Print in the console
-    print(data)
-    print("data with crc check: {0}".format(data_crc8))
-
-    # Append in a file
-    log = open("log.txt", "a")
-    log.write(data + "\n")
-    log.close()
+    client.publish("printRoom/humidity/RH%",
+                   "{:10.2f}".format(aht20.get_humidity()))
+    client.publish("printRoom/temp/celsius",
+                   "{:10.2f}".format(aht20.get_temperature()))
 
     # Wait
-    time.sleep(2)
+    time.sleep(10)
